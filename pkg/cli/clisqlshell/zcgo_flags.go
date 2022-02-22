279024103e4ed29393cb0d3bcaa09ed23f9e2cac
Cockroach TeamCity <teamcity@cockroachlabs.com>
2022-02-22 18:02:28 +0000

    Snapshot v21.2.5-53

---
+++ pkg/cli/clisqlshell/zcgo_flags.go
@@ -0,0 +1,9 @@
+// GENERATED FILE DO NOT EDIT
+
+// +build !make
+
+package clisqlshell
+
+// #cgo CPPFLAGS: -I/go/native/x86_64-pc-linux-gnu/jemalloc/include -I/go/native/x86_64-pc-linux-gnu/krb5/include
+// #cgo LDFLAGS: -L/go/native/x86_64-pc-linux-gnu/jemalloc/lib -L/go/native/x86_64-pc-linux-gnu/libedit/src/.libs -L/go/native/x86_64-pc-linux-gnu/krb5/lib -L/go/native/x86_64-pc-linux-gnu/proj/lib
+import "C"
