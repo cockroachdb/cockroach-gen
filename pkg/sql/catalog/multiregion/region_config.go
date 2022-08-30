cockroachdb/cockroach

sql/multiretion: Add basic validation for zone config extension setting
We added constraints on setting the zone config extension so that it won't break
the original region configs (such as home region and survival goal requirement).

We didn't exhaust all rules but added the following:

1. REGIONAL extensions don't set lease_preferences
2. REGIONAL IN extensions don't set lease_preferences that disagree with their home
region. I.e. After the extension gets applied, the home region is unchanged.
3. REGIONAL IN extensions don't set replica constraints or voter contains that disagree
with their home region.
4. Extensions can't set the number of replicas and voters lower than the ones
required by the database's survival goal.

Note that the validation for zone config extension is different from other
zone config settings in that it will influence the derived zone config for not
just the database, but also the tables and partitions under it. So we need to
ensure the validation pass for all these schemas before truly write the zone
config updates.

fixes #85015

Release justification: important validation for a multiregion funtionality.
Release note: add basic validation for zone config extension setting

pkg/sql/catalog/multiregion/region_config.go
@@ -155,11 +155,30 @@ func (r *RegionConfig) ZoneConfigExtensions() descpb.ZoneConfigExtensions {
 
 // ApplyZoneConfigExtensionForGlobal applies the global table zone configuration
 // extensions to the provided zone configuration, returning the updated config.
-func (r *RegionConfig) ApplyZoneConfigExtensionForGlobal(zc zonepb.ZoneConfig) zonepb.ZoneConfig {
+func (r *RegionConfig) ApplyZoneConfigExtensionForGlobal(
+	zc zonepb.ZoneConfig,
+) (zonepb.ZoneConfig, error) {
 	if ext := r.zoneCfgExtensions.Global; ext != nil {
+		var numVoters, numReplicas int32
+		if zc.NumVoters != nil {
+			numVoters = *zc.NumVoters
+		}
+		if zc.NumReplicas != nil {
+			numReplicas = *zc.NumReplicas
+		}
 		zc = extendZoneCfg(zc, *ext)
+		if zc.NumVoters != nil && *zc.NumVoters < numVoters {
+			return zonepb.ZoneConfig{}, errors.Newf("zone config extension "+
+				"cannot set a voter number lower than the one required for the "+
+				"survival goal: %v with goal %v\n", numVoters, r.SurvivalGoal())
+		}
+		if zc.NumReplicas != nil && *zc.NumReplicas < numReplicas {
+			return zonepb.ZoneConfig{}, errors.Newf("zone config extension "+
+				"cannot set a replica number lower than the one required for the "+
+				"survival goal: %v with goal %v\n", numReplicas, r.SurvivalGoal())
+		}
 	}
-	return zc
+	return zc, nil
 }
 
 // ApplyZoneConfigExtensionForRegionalIn applies the regional table zone
@@ -167,14 +186,44 @@ func (r *RegionConfig) ApplyZoneConfigExtensionForGlobal(zc zonepb.ZoneConfig) z
 // configuration, returning the updated config.
 func (r *RegionConfig) ApplyZoneConfigExtensionForRegionalIn(
 	zc zonepb.ZoneConfig, region catpb.RegionName,
-) zonepb.ZoneConfig {
+) (zonepb.ZoneConfig, error) {
+	var numVoters, numReplicas int32
+	if zc.NumVoters != nil {
+		numVoters = *zc.NumVoters
+	}
+	if zc.NumReplicas != nil {
+		numReplicas = *zc.NumReplicas
+	}
+
 	if ext := r.zoneCfgExtensions.Regional; ext != nil {
+		if ext.LeasePreferences != nil {
+			return zonepb.ZoneConfig{}, errors.New("REGIONAL zone config extensions " +
+				"are not allowed to set lease_preferences")
+		}
 		zc = extendZoneCfg(zc, *ext)
 	}
 	if ext, ok := r.zoneCfgExtensions.RegionalIn[region]; ok {
+		if err := validateZoneConfigExtension(ext, zc, region.String()); err != nil {
+			return zonepb.ZoneConfig{}, err
+		}
 		zc = extendZoneCfg(zc, ext)
 	}
-	return zc
+
+	// TODO(janexing): to ensure that the zone config extension won't break the
+	// survival goal, here we only add restriction on the number of voters and
+	// replicas. We may want to consider adding constraint to their distribution
+	// across zones/regions as well.
+	if zc.NumVoters != nil && *zc.NumVoters < numVoters {
+		return zonepb.ZoneConfig{}, errors.Newf("zone config extension "+
+			"cannot set a voter number lower than the one required for the "+
+			"survival goal: %v with goal %v\n", numVoters, r.SurvivalGoal())
+	}
+	if zc.NumReplicas != nil && *zc.NumReplicas < numReplicas {
+		return zonepb.ZoneConfig{}, errors.Newf("zone config extension "+
+			"cannot set a replica number lower than the one required for the "+
+			"survival goal: %v with goal %v\n", numReplicas, r.SurvivalGoal())
+	}
+	return zc, nil
 }
 
 // "extending" a zone config means having the extension inherit any missing
@@ -467,4 +516,107 @@ func CanDropRegion(name catpb.RegionName, config RegionConfig) error {
 		)
 	}
 	return CanSatisfySurvivalGoal(config.survivalGoal, len(config.regions)-1)
+}
+
+// getHomeRegionConstraintConjunction returns the ConstraintsConjunction from
+// a list of ConstraintsConjunction for the given region name.
+// If we found the constraint for the region, we return the corresponding
+// ConstraintsConjunction and "true" as the second boolean returned value.
+// If not found in the list, it returns an empty lease preference and "false".
+func getHomeRegionConstraintConjunction(
+	constraints []zonepb.ConstraintsConjunction, homeRegion string,
+) (constraintConjunction zonepb.ConstraintsConjunction, found bool) {
+	for _, constraint := range constraints {
+		// TODO(janexing): We should also consider the case with contraints on zone
+		// level.
+		if constraint.Constraints[0].Key == "region" &&
+			constraint.Constraints[0].Value == homeRegion {
+			return constraint, true
+		}
+	}
+	return zonepb.ConstraintsConjunction{}, false
+}
+
+// getHomeRegionLeasePreference returns the lease preference and its index from
+// a list of lease preference for the given region name. If not found in the
+// list, it returns an empty lease preference and -1 as the index.
+func getHomeRegionLeasePreference(
+	leasePreferences []zonepb.LeasePreference, homeRegion string,
+) (zonepb.LeasePreference, int) {
+	for idx, leasePreference := range leasePreferences {
+		// TODO(janexing): We should also consider the case with contraints on zone
+		// level.
+		if leasePreference.Constraints[0].Value == homeRegion {
+			return leasePreference, idx
+		}
+	}
+	return zonepb.LeasePreference{}, -1
+}
+
+// validateZoneConfigExtension ensures that the zone config extensions don't
+// set configs that disagree with their home region.
+func validateZoneConfigExtension(
+	ext zonepb.ZoneConfig, zc zonepb.ZoneConfig, homeRegion string,
+) error {
+	if homeRegion == "" {
+		return nil
+	}
+
+	// Lease preferences.
+	if len(zc.LeasePreferences) > 0 && len(ext.LeasePreferences) > 0 {
+		oriHomeRegionLeasePreference, idx1 := getHomeRegionLeasePreference(zc.LeasePreferences, homeRegion)
+		extHomeRegionLeasePreference, idx2 := getHomeRegionLeasePreference(ext.LeasePreferences, homeRegion)
+		if idx1 != -1 {
+			// If can' find a lease preference for the home region in the zone config
+			// extension's lease preferences.
+			if idx2 == -1 {
+				return errors.Newf("zone config extension cannot unset the home "+
+					"region's lease preference: %v\n", oriHomeRegionLeasePreference)
+			}
+			if idx1 != idx2 {
+				return errors.Newf("zone config extension cannot change priority"+
+					" of lease preference for the home region: %v\n", oriHomeRegionLeasePreference)
+			}
+			if oriHomeRegionLeasePreference.Constraints[0].Type != extHomeRegionLeasePreference.Constraints[0].Type {
+				return errors.Newf("zone config extension %v violates the home"+
+					" region's lease preference: %v\n", extHomeRegionLeasePreference, oriHomeRegionLeasePreference)
+			}
+		}
+	}
+
+	// Replica constraints.
+	if len(zc.Constraints) > 0 && len(ext.Constraints) > 0 {
+		oriHomeRegionReplicaConstraint, ok1 := getHomeRegionConstraintConjunction(zc.Constraints, homeRegion)
+		extHomeRegionReplicaConstraint, ok2 := getHomeRegionConstraintConjunction(ext.Constraints, homeRegion)
+		if ok1 {
+			if !ok2 {
+				return errors.Newf("zone config extension cannot unset the home"+
+					" region's replica constraint: %v\n", oriHomeRegionReplicaConstraint)
+			}
+			if oriHomeRegionReplicaConstraint.Constraints[0].Type != extHomeRegionReplicaConstraint.Constraints[0].Type ||
+				oriHomeRegionReplicaConstraint.NumReplicas > extHomeRegionReplicaConstraint.NumReplicas {
+				return errors.Newf("zone config extension %v violates the home"+
+					" region's replica constraint: %v\n", extHomeRegionReplicaConstraint, oriHomeRegionReplicaConstraint)
+			}
+		}
+	}
+
+	// Voter constraints.
+	if len(zc.VoterConstraints) > 0 && len(ext.VoterConstraints) > 0 {
+		oriHomeRegionVoterConstraint, ok1 := getHomeRegionConstraintConjunction(zc.Constraints, homeRegion)
+		extHomeRegionVoterConstraint, ok2 := getHomeRegionConstraintConjunction(ext.Constraints, homeRegion)
+		if ok1 {
+			if !ok2 {
+				return errors.Newf("zone config extension cannot unset the home"+
+					" region's voter constraint: %v\n", oriHomeRegionVoterConstraint)
+			}
+			if oriHomeRegionVoterConstraint.Constraints[0].Type != extHomeRegionVoterConstraint.Constraints[0].Type ||
+				oriHomeRegionVoterConstraint.NumReplicas > extHomeRegionVoterConstraint.NumReplicas {
+				return errors.Newf("zone config extension %v violates the home"+
+					" region's voter constraint: %v\n", extHomeRegionVoterConstraint, oriHomeRegionVoterConstraint)
+			}
+		}
+	}
+
+	return nil
 }

pkg/sql/region_util.go
@@ -171,8 +171,8 @@ func zoneConfigForMultiRegionDatabase(
 	// database and for each mutable table in database, to prevent them
 	// from violating the multi-region primitive.
 	// https://github.com/cockroachdb/cockroach/issues/85015
-	zc = regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, regionConfig.PrimaryRegion())
-	return zc, nil
+
+	return regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, regionConfig.PrimaryRegion())
 }
 
 // addConstraintsForSuperRegion updates the ZoneConfig.Constraints field such
@@ -283,9 +283,7 @@ func zoneConfigForMultiRegionPartition(
 	zc.LeasePreferences = leasePreferences
 	zc.InheritedLeasePreferences = false
 
-	zc = regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, partitionRegion)
-	return zc, err
-
+	return regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, partitionRegion)
 }
 
 // maxFailuresBeforeUnavailability returns the maximum number of individual
@@ -566,7 +564,10 @@ func zoneConfigForMultiRegionTable(
 			zc.LeasePreferences = leasePreferences
 			zc.InheritedLeasePreferences = false
 
-			zc = regionConfig.ApplyZoneConfigExtensionForGlobal(zc)
+			zc, err = regionConfig.ApplyZoneConfigExtensionForGlobal(zc)
+			if err != nil {
+				return zonepb.ZoneConfig{}, err
+			}
 		}
 		// Inherit lease preference from the database. We do
 		// nothing here because `NewZoneConfig()` already marks the field as
@@ -616,8 +617,7 @@ func zoneConfigForMultiRegionTable(
 		zc.LeasePreferences = leasePreferences
 		zc.InheritedLeasePreferences = false
 
-		zc = regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, affinityRegion)
-		return zc, nil
+		return regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, affinityRegion)
 
 	case *catpb.LocalityConfig_RegionalByRow_:
 		// We purposely do not set anything here at table level - this should be done at
@@ -916,6 +916,27 @@ func ApplyZoneConfigForMultiRegionTable(
 	return err
 }
 
+// GenerateAndValidateZoneConfigForMultiRegionDatabase returns a validated
+// zone config generated by the region config.
+func GenerateAndValidateZoneConfigForMultiRegionDatabase(
+	regionConfig multiregion.RegionConfig,
+) (zonepb.ZoneConfig, error) {
+	// Build a zone config based on the RegionConfig information.
+	dbZoneConfig := zonepb.ZoneConfig{}
+	var err error
+	dbZoneConfig, err = zoneConfigForMultiRegionDatabase(regionConfig)
+	if err != nil {
+		return zonepb.ZoneConfig{}, err
+	}
+
+	// Finally revalidate everything. Validate only the completeZone config.
+	if err := dbZoneConfig.Validate(); err != nil {
+		return zonepb.ZoneConfig{}, pgerror.Wrap(err, pgcode.CheckViolation, "could not validate zone config")
+	}
+
+	return dbZoneConfig, nil
+}
+
 // ApplyZoneConfigFromDatabaseRegionConfig applies a zone configuration to the
 // database using the information in the supplied RegionConfig.
 func ApplyZoneConfigFromDatabaseRegionConfig(
@@ -927,15 +948,11 @@ func ApplyZoneConfigFromDatabaseRegionConfig(
 	descriptors *descs.Collection,
 ) error {
 	// Build a zone config based on the RegionConfig information.
-	dbZoneConfig, err := zoneConfigForMultiRegionDatabase(regionConfig)
+	dbZoneConfig, err := GenerateAndValidateZoneConfigForMultiRegionDatabase(regionConfig)
 	if err != nil {
 		return err
 	}
 
-	// Finally revalidate everything. Validate only the completeZone config.
-	if err := dbZoneConfig.Validate(); err != nil {
-		return pgerror.Wrap(err, pgcode.CheckViolation, "could not validate zone config")
-	}
 	return applyZoneConfigForMultiRegionDatabase(
 		ctx,
 		dbID,
@@ -1053,6 +1070,61 @@ func (p *planner) refreshZoneConfigsForTables(
 			return nil
 		},
 	)
+}
+
+// refreshZoneConfigsForTablesWithValidation is basically the same as
+// p.refreshZoneConfigsForTables(), except that here we first validate the
+// new zone config for all tables before truly write each of the updates.
+func (p *planner) refreshZoneConfigsForTablesWithValidation(
+	ctx context.Context, desc catalog.DatabaseDescriptor, refreshOpts ...refreshZoneConfigOption,
+) error {
+	opts := refreshZoneConfigOptions{
+		filterFunc: func(_ *tabledesc.Mutable) bool { return true },
+	}
+	for _, f := range refreshOpts {
+		f(&opts)
+	}
+
+	regionConfig, err := SynthesizeRegionConfig(ctx, p.txn, desc.GetID(), p.Descriptors())
+	if err != nil {
+		return err
+	}
+
+	var zoneConfigUpdates []*zoneConfigUpdate
+
+	if err := p.forEachMutableTableInDatabase(
+		ctx,
+		desc,
+		func(ctx context.Context, scName string, tbDesc *tabledesc.Mutable) error {
+			if opts.filterFunc(tbDesc) {
+				update, err := prepareZoneConfigForMultiRegionTable(
+					ctx,
+					p.txn,
+					p.ExecCfg(),
+					regionConfig,
+					tbDesc,
+					ApplyZoneConfigForMultiRegionTableOptionTableAndIndexes,
+				)
+				if update == nil || err != nil {
+					return err
+				}
+				zoneConfigUpdates = append(zoneConfigUpdates, update)
+			}
+			return nil
+		},
+	); err != nil {
+		return err
+	}
+	// TODO(janexing): if any write failed, do we roll back? Same question to the
+	// original p.forEachMutableTableInDatabase().
+	for _, update := range zoneConfigUpdates {
+		_, err = writeZoneConfigUpdate(ctx, p.Txn(), p.ExecCfg(), p.Descriptors(), update)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+
 }
 
 // WithOnlyGlobalTables modifies an refreshZoneConfigOptions to only apply to

cockroachdb/cockroach-gen

pkg/sql/catalog/multiregion/region_config.go
@@ -153,28 +153,79 @@ func (r *RegionConfig) ZoneConfigExtensions() descpb.ZoneConfigExtensions {
 	return r.zoneCfgExtensions
 }
 
-// ApplyZoneConfigExtensionForGlobal applies the global table zone configuration
+// ExtendZoneConfigWithGlobal applies the global table zone configuration
 // extensions to the provided zone configuration, returning the updated config.
-func (r *RegionConfig) ApplyZoneConfigExtensionForGlobal(zc zonepb.ZoneConfig) zonepb.ZoneConfig {
+func (r *RegionConfig) ExtendZoneConfigWithGlobal(zc zonepb.ZoneConfig) (zonepb.ZoneConfig, error) {
 	if ext := r.zoneCfgExtensions.Global; ext != nil {
+		var numVoters, numReplicas int32
+		if zc.NumVoters != nil {
+			numVoters = *zc.NumVoters
+		}
+		if zc.NumReplicas != nil {
+			numReplicas = *zc.NumReplicas
+		}
 		zc = extendZoneCfg(zc, *ext)
+		// TODO(janexing): to ensure that the zone config extension won't break the
+		// survival goal, here we only add restriction on the number of voters and
+		// replicas. We may want to consider adding constraint to their distribution
+		// across zones/regions as well.
+		if zc.NumVoters != nil && *zc.NumVoters < numVoters {
+			return zonepb.ZoneConfig{}, errors.Newf("zone config extension "+
+				"cannot set a voter number lower than the one required for the "+
+				"survival goal: %v with goal %v\n", numVoters, r.SurvivalGoal())
+		}
+		if zc.NumReplicas != nil && *zc.NumReplicas < numReplicas {
+			return zonepb.ZoneConfig{}, errors.Newf("zone config extension "+
+				"cannot set a replica number lower than the one required for the "+
+				"survival goal: %v with goal %v\n", numReplicas, r.SurvivalGoal())
+		}
 	}
-	return zc
+	return zc, nil
 }
 
-// ApplyZoneConfigExtensionForRegionalIn applies the regional table zone
+// ExtendZoneConfigWithRegionalIn applies the regional table zone
 // configuration extensions for the provided region to the provided zone
 // configuration, returning the updated config.
-func (r *RegionConfig) ApplyZoneConfigExtensionForRegionalIn(
+func (r *RegionConfig) ExtendZoneConfigWithRegionalIn(
 	zc zonepb.ZoneConfig, region catpb.RegionName,
-) zonepb.ZoneConfig {
+) (zonepb.ZoneConfig, error) {
+	var numVoters, numReplicas int32
+	if zc.NumVoters != nil {
+		numVoters = *zc.NumVoters
+	}
+	if zc.NumReplicas != nil {
+		numReplicas = *zc.NumReplicas
+	}
+
 	if ext := r.zoneCfgExtensions.Regional; ext != nil {
+		if ext.LeasePreferences != nil {
+			return zonepb.ZoneConfig{}, errors.New("REGIONAL zone config extensions " +
+				"are not allowed to set lease_preferences")
+		}
 		zc = extendZoneCfg(zc, *ext)
 	}
 	if ext, ok := r.zoneCfgExtensions.RegionalIn[region]; ok {
+		if err := validateZoneConfigExtension(ext, zc, region.String()); err != nil {
+			return zonepb.ZoneConfig{}, err
+		}
 		zc = extendZoneCfg(zc, ext)
 	}
-	return zc
+
+	// TODO(janexing): to ensure that the zone config extension won't break the
+	// survival goal, here we only add restriction on the number of voters and
+	// replicas. We may want to consider adding constraint to their distribution
+	// across zones/regions as well.
+	if zc.NumVoters != nil && *zc.NumVoters < numVoters {
+		return zonepb.ZoneConfig{}, errors.Newf("zone config extension "+
+			"cannot set a voter number lower than the one required for the "+
+			"survival goal: %v with goal %v\n", numVoters, r.SurvivalGoal())
+	}
+	if zc.NumReplicas != nil && *zc.NumReplicas < numReplicas {
+		return zonepb.ZoneConfig{}, errors.Newf("zone config extension "+
+			"cannot set a replica number lower than the one required for the "+
+			"survival goal: %v with goal %v\n", numReplicas, r.SurvivalGoal())
+	}
+	return zc, nil
 }
 
 // "extending" a zone config means having the extension inherit any missing
@@ -467,4 +518,107 @@ func CanDropRegion(name catpb.RegionName, config RegionConfig) error {
 		)
 	}
 	return CanSatisfySurvivalGoal(config.survivalGoal, len(config.regions)-1)
+}
+
+// getHomeRegionConstraintConjunction returns the ConstraintsConjunction from
+// a list of ConstraintsConjunction for the given region name.
+// If we found the constraint for the region, we return the corresponding
+// ConstraintsConjunction and "true" as the second boolean returned value.
+// If not found in the list, it returns an empty lease preference and "false".
+func getHomeRegionConstraintConjunction(
+	constraints []zonepb.ConstraintsConjunction, homeRegion string,
+) (constraintConjunction zonepb.ConstraintsConjunction, found bool) {
+	for _, constraint := range constraints {
+		// TODO(janexing): We should also consider the case with contraints on zone
+		// level.
+		if constraint.Constraints[0].Key == "region" &&
+			constraint.Constraints[0].Value == homeRegion {
+			return constraint, true
+		}
+	}
+	return zonepb.ConstraintsConjunction{}, false
+}
+
+// getHomeRegionLeasePreference returns the lease preference and its index from
+// a list of lease preference for the given region name. If not found in the
+// list, it returns an empty lease preference and -1 as the index.
+func getHomeRegionLeasePreference(
+	leasePreferences []zonepb.LeasePreference, homeRegion string,
+) (zonepb.LeasePreference, int) {
+	for idx, leasePreference := range leasePreferences {
+		// TODO(janexing): We should also consider the case with contraints on zone
+		// level.
+		if leasePreference.Constraints[0].Value == homeRegion {
+			return leasePreference, idx
+		}
+	}
+	return zonepb.LeasePreference{}, -1
+}
+
+// validateZoneConfigExtension ensures that the zone config extensions don't
+// set configs that disagree with their home region.
+func validateZoneConfigExtension(
+	ext zonepb.ZoneConfig, zc zonepb.ZoneConfig, homeRegion string,
+) error {
+	if homeRegion == "" {
+		return nil
+	}
+
+	// Lease preferences.
+	if len(zc.LeasePreferences) > 0 && len(ext.LeasePreferences) > 0 {
+		oriHomeRegionLeasePreference, idx1 := getHomeRegionLeasePreference(zc.LeasePreferences, homeRegion)
+		extHomeRegionLeasePreference, idx2 := getHomeRegionLeasePreference(ext.LeasePreferences, homeRegion)
+		if idx1 != -1 {
+			// If can' find a lease preference for the home region in the zone config
+			// extension's lease preferences.
+			if idx2 == -1 {
+				return errors.Newf("zone config extension cannot unset the home "+
+					"region's lease preference: %v\n", oriHomeRegionLeasePreference)
+			}
+			if idx1 != idx2 {
+				return errors.Newf("zone config extension cannot change priority"+
+					" of lease preference for the home region: %v\n", oriHomeRegionLeasePreference)
+			}
+			if oriHomeRegionLeasePreference.Constraints[0].Type != extHomeRegionLeasePreference.Constraints[0].Type {
+				return errors.Newf("zone config extension %v violates the home"+
+					" region's lease preference: %v\n", extHomeRegionLeasePreference, oriHomeRegionLeasePreference)
+			}
+		}
+	}
+
+	// Replica constraints.
+	if len(zc.Constraints) > 0 && len(ext.Constraints) > 0 {
+		oriHomeRegionReplicaConstraint, ok1 := getHomeRegionConstraintConjunction(zc.Constraints, homeRegion)
+		extHomeRegionReplicaConstraint, ok2 := getHomeRegionConstraintConjunction(ext.Constraints, homeRegion)
+		if ok1 {
+			if !ok2 {
+				return errors.Newf("zone config extension cannot unset the home"+
+					" region's replica constraint: %v\n", oriHomeRegionReplicaConstraint)
+			}
+			if oriHomeRegionReplicaConstraint.Constraints[0].Type != extHomeRegionReplicaConstraint.Constraints[0].Type ||
+				oriHomeRegionReplicaConstraint.NumReplicas > extHomeRegionReplicaConstraint.NumReplicas {
+				return errors.Newf("zone config extension %v violates the home"+
+					" region's replica constraint: %v\n", extHomeRegionReplicaConstraint, oriHomeRegionReplicaConstraint)
+			}
+		}
+	}
+
+	// Voter constraints.
+	if len(zc.VoterConstraints) > 0 && len(ext.VoterConstraints) > 0 {
+		oriHomeRegionVoterConstraint, ok1 := getHomeRegionConstraintConjunction(zc.Constraints, homeRegion)
+		extHomeRegionVoterConstraint, ok2 := getHomeRegionConstraintConjunction(ext.Constraints, homeRegion)
+		if ok1 {
+			if !ok2 {
+				return errors.Newf("zone config extension cannot unset the home"+
+					" region's voter constraint: %v\n", oriHomeRegionVoterConstraint)
+			}
+			if oriHomeRegionVoterConstraint.Constraints[0].Type != extHomeRegionVoterConstraint.Constraints[0].Type ||
+				oriHomeRegionVoterConstraint.NumReplicas > extHomeRegionVoterConstraint.NumReplicas {
+				return errors.Newf("zone config extension %v violates the home"+
+					" region's voter constraint: %v\n", extHomeRegionVoterConstraint, oriHomeRegionVoterConstraint)
+			}
+		}
+	}
+
+	return nil
 }

pkg/sql/region_util.go
@@ -171,8 +171,8 @@ func zoneConfigForMultiRegionDatabase(
 	// database and for each mutable table in database, to prevent them
 	// from violating the multi-region primitive.
 	// https://github.com/cockroachdb/cockroach/issues/85015
-	zc = regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, regionConfig.PrimaryRegion())
-	return zc, nil
+
+	return regionConfig.ExtendZoneConfigWithRegionalIn(zc, regionConfig.PrimaryRegion())
 }
 
 // addConstraintsForSuperRegion updates the ZoneConfig.Constraints field such
@@ -283,9 +283,7 @@ func zoneConfigForMultiRegionPartition(
 	zc.LeasePreferences = leasePreferences
 	zc.InheritedLeasePreferences = false
 
-	zc = regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, partitionRegion)
-	return zc, err
-
+	return regionConfig.ExtendZoneConfigWithRegionalIn(zc, partitionRegion)
 }
 
 // maxFailuresBeforeUnavailability returns the maximum number of individual
@@ -566,7 +564,10 @@ func zoneConfigForMultiRegionTable(
 			zc.LeasePreferences = leasePreferences
 			zc.InheritedLeasePreferences = false
 
-			zc = regionConfig.ApplyZoneConfigExtensionForGlobal(zc)
+			zc, err = regionConfig.ExtendZoneConfigWithGlobal(zc)
+			if err != nil {
+				return zonepb.ZoneConfig{}, err
+			}
 		}
 		// Inherit lease preference from the database. We do
 		// nothing here because `NewZoneConfig()` already marks the field as
@@ -616,8 +617,7 @@ func zoneConfigForMultiRegionTable(
 		zc.LeasePreferences = leasePreferences
 		zc.InheritedLeasePreferences = false
 
-		zc = regionConfig.ApplyZoneConfigExtensionForRegionalIn(zc, affinityRegion)
-		return zc, nil
+		return regionConfig.ExtendZoneConfigWithRegionalIn(zc, affinityRegion)
 
 	case *catpb.LocalityConfig_RegionalByRow_:
 		// We purposely do not set anything here at table level - this should be done at
@@ -916,6 +916,27 @@ func ApplyZoneConfigForMultiRegionTable(
 	return err
 }
 
+// GenerateAndValidateZoneConfigForMultiRegionDatabase returns a validated
+// zone config generated by the region config.
+func GenerateAndValidateZoneConfigForMultiRegionDatabase(
+	regionConfig multiregion.RegionConfig,
+) (zonepb.ZoneConfig, error) {
+	// Build a zone config based on the RegionConfig information.
+	dbZoneConfig := zonepb.ZoneConfig{}
+	var err error
+	dbZoneConfig, err = zoneConfigForMultiRegionDatabase(regionConfig)
+	if err != nil {
+		return zonepb.ZoneConfig{}, err
+	}
+
+	// Finally revalidate everything. Validate only the completeZone config.
+	if err := dbZoneConfig.Validate(); err != nil {
+		return zonepb.ZoneConfig{}, pgerror.Wrap(err, pgcode.CheckViolation, "could not validate zone config")
+	}
+
+	return dbZoneConfig, nil
+}
+
 // ApplyZoneConfigFromDatabaseRegionConfig applies a zone configuration to the
 // database using the information in the supplied RegionConfig.
 func ApplyZoneConfigFromDatabaseRegionConfig(
@@ -927,15 +948,11 @@ func ApplyZoneConfigFromDatabaseRegionConfig(
 	descriptors *descs.Collection,
 ) error {
 	// Build a zone config based on the RegionConfig information.
-	dbZoneConfig, err := zoneConfigForMultiRegionDatabase(regionConfig)
+	dbZoneConfig, err := GenerateAndValidateZoneConfigForMultiRegionDatabase(regionConfig)
 	if err != nil {
 		return err
 	}
 
-	// Finally revalidate everything. Validate only the completeZone config.
-	if err := dbZoneConfig.Validate(); err != nil {
-		return pgerror.Wrap(err, pgcode.CheckViolation, "could not validate zone config")
-	}
 	return applyZoneConfigForMultiRegionDatabase(
 		ctx,
 		dbID,
@@ -1053,6 +1070,65 @@ func (p *planner) refreshZoneConfigsForTables(
 			return nil
 		},
 	)
+}
+
+// refreshZoneConfigsForTablesWithValidation is basically the same as
+// p.refreshZoneConfigsForTables(), except that here we first validate the
+// new zone config for all tables before truly write each of the updates.
+func (p *planner) refreshZoneConfigsForTablesWithValidation(
+	ctx context.Context, desc catalog.DatabaseDescriptor, refreshOpts ...refreshZoneConfigOption,
+) error {
+	opts := refreshZoneConfigOptions{
+		filterFunc: func(_ *tabledesc.Mutable) bool { return true },
+	}
+	for _, f := range refreshOpts {
+		f(&opts)
+	}
+
+	regionConfig, err := SynthesizeRegionConfig(ctx, p.txn, desc.GetID(), p.Descriptors())
+	if err != nil {
+		return err
+	}
+
+	var zoneConfigUpdates []*zoneConfigUpdate
+
+	// For each table in the database, we check validate the updated zone config,
+	// and if there's no error, we add the zone config update in zoneConfigUpdates.
+	if err := p.forEachMutableTableInDatabase(
+		ctx,
+		desc,
+		func(ctx context.Context, scName string, tbDesc *tabledesc.Mutable) error {
+			if opts.filterFunc(tbDesc) {
+				update, err := prepareZoneConfigForMultiRegionTable(
+					ctx,
+					p.txn,
+					p.ExecCfg(),
+					regionConfig,
+					tbDesc,
+					ApplyZoneConfigForMultiRegionTableOptionTableAndIndexes,
+				)
+				if update == nil || err != nil {
+					return err
+				}
+				zoneConfigUpdates = append(zoneConfigUpdates, update)
+			}
+			return nil
+		},
+	); err != nil {
+		return err
+	}
+	// With all tables scanned and validated for the updated zone config, we now
+	// can finally write the changes.
+	// TODO(janexing): if any write failed, do we roll back? Same question to the
+	// original p.forEachMutableTableInDatabase().
+	for _, update := range zoneConfigUpdates {
+		_, err = writeZoneConfigUpdate(ctx, p.Txn(), p.ExecCfg(), p.Descriptors(), update)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+
 }
 
 // WithOnlyGlobalTables modifies an refreshZoneConfigOptions to only apply to